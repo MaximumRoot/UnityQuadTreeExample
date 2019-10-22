68e59ea5777fef78f9409fe5369f2b729b3b7990
Choe Daeguen <asdf123ggg@gmail.com>
2019-10-22 16:36:29 +0900

    QuadTree 사용 예시 스크립트

---
+++ Monster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : SurvColliderMonoBehaviour
+{
+    private float dx, dz;
+
+    public void Init(QuadTree quadTree)
+    {
+        SetQuadTree(quadTree);
+    }
+    void Start()
+    {
+        float theta = Random.Range(0f, 3.14f);
+        dx = 2f*Mathf.Cos(theta);
+        dz = 2f*Mathf.Sin(theta);
+    }
+    void Update()
+    {
+        transform.Translate(dx*Time.deltaTime, 0, dz*Time.deltaTime);
+        if (transform.position.x > 20f || transform.position.x < -20f)
+        {
+            dx = -dx;
+        }
+        if (transform.position.z > 20f || transform.position.z < -20f)
+        {
+            dz = -dz;
+        }
+        quadTree.Move(this);
+    }
+}
---
+++ MonsterGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterGenerator : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+    private QuadTree quadTree;
+    private int id=0;
+
+    void Start()
+    {
+        quadTree = new QuadTree(0, 0, 40, 40);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float nx = Random.Range(-20f, 20f);
+            float nz = Random.Range(-20f, 20f);
+            GameObject newMonster = Instantiate(monsterPrefab) as GameObject;
+            newMonster.name = id.ToString();
+            Monster script = newMonster.GetComponent<Monster>();
+            script.Init(quadTree);
+            newMonster.transform.position = new Vector3(nx, 0, nz);
+            id += 1;
+
+            quadTree.Add(script);
+        }
+    }
+}
